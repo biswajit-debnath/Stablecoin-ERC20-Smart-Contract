705a7338913227881735d5fdbf420555a8bf8688
biswajit-debnath <biswajitdebnath405@gmail.com>
2025-03-14 13:15:49 +0530

    Feature: Health factor related functions added

--- src/SCEngine.sol
+++ src/SCEngine.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {StableCoin} from "./StableCoin.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract SCEngine {
     /* Errors */
@@ -11,15 +12,24 @@ contract SCEngine {
     error SCEngine__TokenTypeNotApprovedForCollateral(address token);
     error SCEngine__TransferFailed();
     error SCEngine__mintingAmountMustBeMoreThanZero();
+    error SCEngine__HealthFactorIsBroken();
 
     /* Events */
     event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
 
     /* State Variables */
+    uint256 private constant THRESHOLD_VALUE_FOR_HEALTH_FACTOR = 150;
+    uint256 private constant THRESHOLD_PRECISION_VALUE_FOR_HEALTH_FACTOR = 100;
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private constant PRICE_FEED_PRECISION = 1e10;
+
     StableCoin private immutable i_scoin;
     mapping(address => bool) private s_allowedTokens;
     mapping(address => mapping(address => uint256)) private s_userCollateralBalances;
     mapping(address => uint256) private s_userSCoinBalance;
+    mapping(address => address) private s_collateralToPriceFeeds;
+
+    
 
 
     constructor(StableCoin _scoinAddress) {
@@ -55,9 +65,53 @@ contract SCEngine {
     }
 
 
-    function revertIfHealthFactorBroken() internal view {
+    function revertIfHealthFactorBroken() internal {
         // Get the user's health factor
+        uint256 userHealthFactor = getHealthFactor();
         // If it's broken, revert
+        if (userHealthFactor < 1) {
+            revert SCEngine__HealthFactorIsBroken();
+        }
+    }
+
+    function getHealthFactor() public returns(uint256) {
+        // Get the current token minted to user
+        uint256 totalTokensMinted = s_userSCoinBalance[msg.sender];
+
+        // Get the total collateral deposited in dollars
+        uint256 totalCollateralVal = getUserTotalCollateralValInDollars();
+
+        // Convert token amount to Wei precision (1e18)
+        uint256 tokenValueInWei = totalTokensMinted * PRECISION_FACTOR;
+
+        // Apply the threshold multiplier (150%)
+        uint256 adjustedForThreshold = tokenValueInWei * THRESHOLD_VALUE_FOR_HEALTH_FACTOR;
+
+        // Adjust by threshold precision (divide by 100 to get actual percentage)
+        uint256 collateralNeededAdjustingThreshold = adjustedForThreshold / THRESHOLD_PRECISION_VALUE_FOR_HEALTH_FACTOR;
+
+        return collateralNeededAdjustingThreshold;
+    }
+
+
+    function getValueOfGivenCollateralInDollar(address _collateralAddress) public returns(uint256) {
+        // Get total amount of collateral deposited by the user based on the colleteral address
+        uint256 totalCollateralAmount = s_userCollateralBalances[_collateralAddress];
+        uint256 collateralAmountInWei = totalCollateralAmount * PRECISION_FACTOR;
+
+        // Get the price feed address based on collateral address
+        address priceFeedAddress = s_collateralToPriceFeeds[_collateralAddress];
+
+        // Get the price feed data from Chainlink price feed
+        (, int256 price,,,) = AggregatorV3Interface(priceFeedAddress).latestRoundData();
+
+        uint256 normalizedPrice = uint256(price) * PRICE_FEED_PRECISION;
+
+        // Multiple the price feed value with the total amount to the total value
+        uint256 totalValue = collateralAmountInWei * normalizedPrice;
+        uint256 adjustedCollateralValue = totalValue / PRECISION_FACTOR;
+
+        return adjustedCollateralValue;
     }
 
 }
